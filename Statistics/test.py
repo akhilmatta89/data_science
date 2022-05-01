7205d14ee69f573151930c480bc58056652a50a2
akhilmatta89 <67899429+akhilmatta89@users.noreply.github.com>
2022-05-01 09:36:20 +0530

    Added some code

--- Statistics/test.py
+++ Statistics/test.py
@@ -1,1 +1,9 @@
-print('hi')
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+
+indian_imdb_url = 'https://www.imdb.com/india/top-rated-indian-movies/'
+imdb_response = requests.get(indian_imdb_url)
+imdb_response
+indian_imdb_soup = BeautifulSoup(imdb_response.content, 'html.parser')
